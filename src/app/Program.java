f61f1057b101c6ec26d86be6caf636f9655fd1dd
nandcdias <fernando_c_dias@outlook.com>
2020-05-10 19:31:44 -0300

    Criação da camada chess e impressão do tabuleiro

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,12 @@
 package app;
 
+import chess.ChessMatch;
+
 public class Program {
-    
+    public static void main(String[] args) {
+        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        
+    }
 }
